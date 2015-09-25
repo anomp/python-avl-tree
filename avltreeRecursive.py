82f364c0e701123cc0405909aeed1947bddff367
anomp <a_mpa@outlook.com>
2015-09-25 21:22:10 +0300

    added deletion methods
    Added deletion feature
    Updated README file with mine .The old one is on old.README

--- avltreeRecursive.py
+++ avltreeRecursive.py
@@ -1,4 +1,4 @@
-myavllist = [150,40,900,300,700,60,500,160,450,600]
+myavllist = [150,40,900,300,700,60,500,160,450,600,30,80,430]
 
 class Node(object):
     def __init__(self,key,parent=None):
@@ -15,7 +15,29 @@ class Node(object):
             return True
         else:
             return False
+    
+    def leafDeletion(self):
+        #just make its parent pointing to none and update its parent hight
+        par = self.parent
+        if par.L_child is not None and par.L_child is self:
+            par.L_child = None
+            par.L_hight = 0
+        else:
+            par.R_child = None
+            par.R_hight = 0
 
+
+    def updateHight(self):
+        if self.L_child is not None:
+            self.L_hight = self.L_child.hight + 1
+        else:
+            self.L_hight = 0
+        if self.R_child is not None:
+            self.R_hight = self.R_child.hight + 1
+        else:
+            self.R_hight = 0
+        self.hight = max(self.L_hight,self.R_hight)
+    
     def isRoot(self):
         if self.parent is None:
             return True
@@ -166,12 +188,70 @@ class AVLtree(object):
         if cur_node.balance() > 1 or cur_node.balance() < -1:
             self.reBalance(cur_node)
 
+    def swapNodeData(self,node1,node2):
+        tempKey = node1.key
+        node1.key = node2.key
+        node2.key = tempKey
+
+
+    def deleteNode(self,s_key,cur_node = None):
+        if cur_node is None:
+            cur_node = self.rootNode
+        if s_key < cur_node.key:
+            self.deleteNode(s_key,cur_node.L_child)
+        elif s_key > cur_node.key:
+            self.deleteNode(s_key,cur_node.R_child)
+        elif s_key == cur_node.key:
+            #key to delete found.Start the deletion process
+            #if node is a leaf just make its parent pointing to none
+            if cur_node.isLeaf():
+                cur_node.leafDeletion()
+            else:
+                self.findProcAndDel(cur_node.L_child,cur_node)
+        else:
+            print "FAILURE:key not found"
+            return
+        cur_node.updateHight()
+        if cur_node.balance() > 1 or cur_node.balance() < -1:
+            self.reBalance(cur_node)
+        
+    def findProcAndDel(self,cnode,dnode):
+        if cnode.R_child is not None:
+             self.findProcAndDel(cnode.R_child,dnode)
+        else:
+            if cnode.isLeaf():
+                cnode.leafDeletion()
+            else:
+                cnode.parent.R_child = cnode.L_child
+                cnode.L_child.parent = cnode.parent
+                cnode.parent.R_hight -= 1
+            self.swapNodeData(cnode,dnode)
+        cnode.updateHight()
+        if cnode.balance() > 1 or cnode.balance() < -1:
+            self.reBalance(cnode)
+         
 
 
 myavl = AVLtree()
+#myavl.rootNode = Node(150,None) #initialize the root node
 for i in myavllist:
     myavl.insertNode(i)
 myavl.accessNode(600)
+myavl.deleteNode(500)
+myavl.accessNode(430)
 
-
+# myavl2 = AVLtree()
+# myavl2.rootNode = Node(3,None)
+# myavl2.insertNode(1)
+# myavl2.insertNode(2)
+# #myavl2.reBalance(myavl2.rootNode)
+# myavl2.accessNode(1)
+"""
+sources
+http://interactivepython.org/runestone/static/pythonds/Trees/AVLTreeImplementation.html
+https://www.cs.usfca.edu/~galles/visualization/AVLtree.html
+http://www.cise.ufl.edu/~nemo/cop3530/AVL-Tree-Rotations.pdf
+Deletion:
+http://www.cs.toronto.edu/~krueger/cscB63h/lectures/tut03.txt
+"""
 
